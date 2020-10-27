856b66988cfcd650e897ae9d92bd8efad7669abd
Paweł Osipczuk <pawelosipczuk49@gmail.com>
2020-10-27 22:51:30 +0100

    Add NumberGenerator and beans.xml

--- core/src/main/java/org/example/Main.java
+++ core/src/main/java/org/example/Main.java
@@ -2,11 +2,30 @@ package org.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final String CONFIG_LOCATION = "beans.xml";
 
     public static void main(String[] args) {
         log.info("Guess The Number Game");
+
+        // create context (container)
+        ConfigurableApplicationContext context
+                = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numberGenerator
+                = context.getBean("numberGenerator", NumberGenerator.class);
+
+        // call method next() to get a random number
+        int number = numberGenerator.next();
+
+        // log generated number
+        log.info("number = {}", number);
+
+        // close context (container)
+        context.close();
     }
 }
---
+++ core/src/main/java/org/example/NumberGenerator.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface NumberGenerator {
+    int next();
+
+    int getMaxNumber();
+}
